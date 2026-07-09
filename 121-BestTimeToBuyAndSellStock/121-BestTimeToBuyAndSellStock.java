144b01dafb5a7a7cfcdeb0aafe46469c53e7d3d5
HariHaran133 <sreehariharan789@gmail.com>
2026-07-09 10:06:48 +0530

    Create 121-BestTimeToBuyAndSellStock

---
+++ 121-BestTimeToBuyAndSellStock/121-BestTimeToBuyAndSellStock.java
@@ -0,0 +1,18 @@
+// Last updated: 09/07/2026, 10:06:47
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int maxprofit = 0;
+
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]<min){
+                min = prices[i];
+            }
+            else if(prices[i]-min > maxprofit){
+                maxprofit = prices[i]-min;
+            }
+        }
+        return maxprofit;
+        
+    }
+}
